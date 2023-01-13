5948c506eafecc0b741590612d2a388cc63d9b28
Hassan <Hassan@10.0.0.201>
2023-01-12 19:11:54 -0500

    Add two methods

---
+++ src/com/noorteck/git/day2/Main.java
@@ -0,0 +1,15 @@
+package com.noorteck.git.day2;
+
+public class Main {
+
+	public static void main(String [] args) {
+		System.out.println("Git hub");
+		
+		
+	}
+	
+	public static void addtwoNumbers(int a, int b) {
+		System.out.println(a+b);
+		
+	}
+}
